5531a85f6e0f7afbca533b605c5367f1f7795af2
Shashi Preetham Awari <109402358+Shashiawari@users.noreply.github.com>
2024-10-08 11:57:49 +0530

    Add files via upload

--- app/component/Navbar/Navabr.jsx
+++ app/component/Navbar/Navabr.jsx
@@ -8,20 +8,32 @@ const Navabr = () => {
     <div>
       <nav className="navbar   ">
         <div className="container-fluid p-2">
-          <a className="navbar-brand" href="#">
-            Awari
-          </a>
+          <div className="d-flex justify-content-center align-items-center" style={{display:"flex", gap:"10px"}}>
+            <button
+              className="navbar-toggler d-lg-none d-md-none d-sm-block"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <a className="navbar-brand" href="#">
+              Awari
+            </a>
+          </div>
           <div className="searchdiv">
             {!isFocused && (
               <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 10-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 001.415-1.414l-3.85-3.85a1 1 0 00-.115-.1zM12 6.5a5.5 5.5 0 11-11 0 5.5 5.5 0 0111 0"></path>
-            </svg>
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 10-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 001.415-1.414l-3.85-3.85a1 1 0 00-.115-.1zM12 6.5a5.5 5.5 0 11-11 0 5.5 5.5 0 0111 0"></path>
+              </svg>
             )}
             <input
               className="search"
@@ -33,55 +45,65 @@ const Navabr = () => {
           </div>
           <div className="icons  ">
             <div className="heart ">
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="26"
-            height="26"
-            fill="currentColor"
-            className="bi bi-heart"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 01.176-.17C12.72-3.042 23.333 4.867 8 15"></path>
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                fill="currentColor"
+                className="bi bi-heart"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 01.176-.17C12.72-3.042 23.333 4.867 8 15"></path>
+              </svg>
               <p>wishlist</p>
             </div>
             <div className="cart text-center ">
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="26"
-            height="26"
-            fill="currentColor"
-            className="bi bi-cart"
-            viewBox="0 0 16 16"
-          >
-            <path d="M0 1.5A.5.5 0 01.5 1H2a.5.5 0 01.485.379L2.89 3H14.5a.5.5 0 01.491.592l-1.5 8A.5.5 0 0113 12H4a.5.5 0 01-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 01-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 100 4 2 2 0 000-4m7 0a2 2 0 100 4 2 2 0 000-4m-7 1a1 1 0 110 2 1 1 0 010-2m7 0a1 1 0 110 2 1 1 0 010-2"></path>
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                fill="currentColor"
+                className="bi bi-cart"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 1.5A.5.5 0 01.5 1H2a.5.5 0 01.485.379L2.89 3H14.5a.5.5 0 01.491.592l-1.5 8A.5.5 0 0113 12H4a.5.5 0 01-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 01-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 100 4 2 2 0 000-4m7 0a2 2 0 100 4 2 2 0 000-4m-7 1a1 1 0 110 2 1 1 0 010-2m7 0a1 1 0 110 2 1 1 0 010-2"></path>
+              </svg>
               <p className="text-center">cart</p>
             </div>
             <div className="loginsvg">
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="26"
-            height="26"
-            fill="currentColor"
-            className="bi bi-person"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 8a3 3 0 100-6 3 3 0 000 6m2-3a2 2 0 11-4 0 2 2 0 014 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"></path>
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                fill="currentColor"
+                className="bi bi-person"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 8a3 3 0 100-6 3 3 0 000 6m2-3a2 2 0 11-4 0 2 2 0 014 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"></path>
+              </svg>
               <p>account</p>
             </div>
           </div>
-          <button
-            className="navbar-toggler d-lg-none d-md-none d-sm-block"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
+          <div className="searchdiv2">
+          {!isFocused && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 10-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 001.415-1.414l-3.85-3.85a1 1 0 00-.115-.1zM12 6.5a5.5 5.5 0 11-11 0 5.5 5.5 0 0111 0"></path>
+            </svg>
+          )}
+          <input
+            className="search"
+            type="search"
+            placeholder={!isFocused ? "Search" : ""}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          />
+        </div>
           <div
             className="offcanvas offcanvas-end"
             tabIndex={-1}
@@ -89,8 +111,16 @@ const Navabr = () => {
             aria-labelledby="offcanvasNavbarLabel"
           >
             <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                Offcanvas
+              <h5
+                style={{
+                  fontFamily: "log",
+                  fontWeight: "bold",
+                  fontSize: "40px",
+                }}
+                className="offcanvas-title"
+                id="offcanvasNavbarLabel"
+              >
+                Awari
               </h5>
               <button
                 type="button"
@@ -108,7 +138,7 @@ const Navabr = () => {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">
-                    Link
+                    About
                   </a>
                 </li>
                 <li className="nav-item dropdown">
@@ -119,17 +149,17 @@ const Navabr = () => {
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
                   >
-                    Dropdown
+                    Find More
                   </a>
                   <ul className="dropdown-menu">
                     <li>
                       <a className="dropdown-item" href="#">
-                        Action
+                        Men
                       </a>
                     </li>
                     <li>
                       <a className="dropdown-item" href="#">
-                        Another action
+                        Women
                       </a>
                     </li>
                     <li>
@@ -137,7 +167,7 @@ const Navabr = () => {
                     </li>
                     <li>
                       <a className="dropdown-item" href="#">
-                        Something else here
+                        View
                       </a>
                     </li>
                   </ul>
--- app/component/corousal/corousal.jsx
+++ app/component/corousal/corousal.jsx
@@ -1,22 +1,45 @@
 import React from "react";
-import "./corousal.css"
+import "./corousal.css";
+import Link from "next/link";
 const Corousal = () => {
   return (
-    <div className="cour">
+    <div className="cour d-flex flex-column ">
+      <div className="ctext w-100 mb-5">
+        <Link href="#">Men</Link>
+        <Link href="#">Women</Link>
+        <Link href="#">smart watches</Link>
+        <Link href="#">premium watches</Link>
+        <Link href="#">international brands</Link>
+        <Link href="#">watches</Link>
+        <Link href="#">sale</Link>
+        <Link href="#">services</Link>
+        <Link href="#">Accessories</Link>
+        <Link href="#">gifting</Link>
+      </div>
+      <div className="ctext2 w-100 mb-5">
+        <Link href="#">Men</Link>
+        <Link href="#">Women</Link>
+        <Link href="#">smart watches</Link>
+        <Link href="#">premium watches</Link>
+        <Link href="#">more</Link>
+      </div>
       <div
         id="carouselExampleAutoplaying"
-        className="carousel slide"
+        className="carousel slide large"
         data-bs-ride="carousel"
       >
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src="c1.png" className="d-block w-100" alt="..." />
+            <img src="c1.png" className="d-block w-100 img-fluid" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="c1.png" className="d-block w-100" alt="..." />
+            <img src="c2.png" className="d-block w-100 imf-fluid" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="c1.png" className="d-block w-100" alt="..." />
+            <img src="c3.png" className="d-block w-100 imf-fluid" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="c4.png" className="d-block w-100 imf-fluid" alt="..." />
           </div>
         </div>
         <button
@@ -37,6 +60,63 @@ const Corousal = () => {
           <span className="carousel-control-next-icon" aria-hidden="true" />
           <span className="visually-hidden">Next</span>
         </button>
+      </div>
+      <div className="small">
+        {" "}
+        <div
+          id="carouselExampleAutoplaying"
+          className="carousel slide "
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src="cc2.png"
+                className="d-block w-100 img-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="cc3.png"
+                className="d-block w-100 imf-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="cc4.png"
+                className="d-block w-100 imf-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="cc5.png"
+                className="d-block w-100 imf-fluid"
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </div>
   );
